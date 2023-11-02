55743ae6adb075cb9903b3252347c332a2dcd762
fredericodgv <fredericodgv@gmail.com>
2023-11-02 15:54:54 -0300

    Cria Prefabs para 3 inimigos

---
+++ Assets/Scripts/ChainController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate Around Head
+        transform.RotateAround(transform.parent.position, Vector3.forward, 45 * Time.deltaTime);
+    }
+}
